7546a9fd5c3f96df09c45d166c12e92db636f1bd
ClaytonTDM <claytontdm@gmail.com>
2022-10-31 02:41:53 -0400

    Hopefully fixed mobile support

--- assets/searchpage.js
+++ assets/searchpage.js
@@ -48,5 +48,9 @@ function getUrlParameter(sParam) {
       console.log('Device: Desktop')
       } else {
       console.log('Device: Mobile');
-      
+document.getElementById('searcharea').innerHTML = `<form action="/search" id="searcharea" style="padding-top:20px;"><img src="/assets/img/logo.png" style="width: 50%;" class="centre"><br>
+<div class="input-group" style="padding-left: 30px; padding-right: 30px; width:100%;">
+<input type="search" class="form-control Post_Description_Text" name="q" id="searchbox" style="resize:none;" rows="1" placeholder="Search the web...">
+<div class="input-group-append"> <button class="btn btn-secondary" type="submit" style="height:38px;width:40.86px;"> <i class="fa fa-search"></i> </button> </div>
+</div></form>`
     }
