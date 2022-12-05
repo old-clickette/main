c01aee94e456c46561b752033ea4786814a4107d
ClaytonTDM <claytontdm@gmail.com>
2022-12-05 00:49:14 -0500

    clicketteairlines

--- assets/eastereggs.js
+++ assets/eastereggs.js
@@ -207,18 +207,29 @@ function springyEmojiCursor(options) {
     function forceSpecialDate() {
     new springyEmojiCursor();
     }
-    var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
-    var current = 0;
-    var keyHandler = function (event) {
-    if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
-        current = 0;
-        return;
-    }
-    current++;
-    if (pattern.length === current) {
-        current = 0;
-    }
-    };
+    var pattern = ['c', 'l', 'i', 'c', 'k', 'e', 't', 't', 'e', 'a', 'i', 'r', 'l', 'i', 'n', 'e', 's'];
+var current = 0;
 
+var keyHandler = function (event) {
+
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+		current = 0;
+		window.alert('You found it!');
+	}
+
+};
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
 
     
